5bb1c40c1d0b5e36dc65bbd436a48b2d154d739b
Kristopher Clark <krisclarkdev@krisbox.org>
2018-07-19 10:02:10 -0500

    Using ENV variable for slack token

--- libs/Chatbot.js
+++ libs/Chatbot.js
@@ -8,8 +8,8 @@ const { WebClient } = require('@slack/client');
 let triggers = ['wf-slackbot'];
 let nlp = require('./NLP.js');
 
-const rtm = new RTMClient(slack_oauth);
-const web = new WebClient(slack_oauth);
+const rtm = new RTMClient(process.env.SLACK_OAUTH);
+//const web = new WebClient(slack_oauth);
 
 exports.Chatbot = class {
   constructor() {
@@ -19,40 +19,6 @@ exports.Chatbot = class {
 
 
 let sendMessage = function(message_id, message, attach) {
-    let rrr = {
-        "text": "Choose a game to play",
-        "fallback": "You are unable to choose a game",
-        "callback_id": "wopr_game",
-        "color": "#3AA3E3",
-        "attachment_type": "default",
-        "actions": [
-            {
-                "name": "game",
-                "text": "Chess",
-                "type": "button",
-                "value": "chess"
-            },
-            {
-                "name": "game",
-                "text": "Falken's Maze",
-                "type": "button",
-                "value": "maze"
-            },
-            {
-                "name": "game",
-                "text": "Thermonuclear War",
-                "style": "danger",
-                "type": "button",
-                "value": "war",
-                "confirm": {
-                    "title": "Are you sure?",
-                    "text": "Wouldn't you prefer a good game of chess?",
-                    "ok_text": "Yes",
-                    "dismiss_text": "No"
-                }
-            }
-        ]
-    }
     web.chat.postMessage({ channel: message_id, text: message, attachments: attach})
         .then((res) => {
             // `res` contains information about the posted message
